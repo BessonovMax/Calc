64395c69436375efbb2ceb9b32917fa2601f4ee4
Maks Bessonov <max8essonov@gmail.com>
2025-04-01 18:16:23 +0500

    add clear entry functionality and improve division by zero handling in calculator

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ let secondNum = "";
 let operator = "";
 let gottenResult = false;
 let display = document.querySelector(".display");
+display.textContent = "0";
 let equalBtn = document.querySelector(".equal");
 let numBtns = document.querySelectorAll(".number");
 let operateBtns = document.querySelectorAll(".operator");
 let clearBtn = document.querySelector(".clear-btn");
+let clearEntryBtn = document.querySelector(".clear-entry-btn");
 let symbol = document.querySelector(".operator-symbol");
 
 function addNumbers(a, b) {
@@ -44,14 +46,45 @@ clearBtn.addEventListener("click", () => {
   window.location.reload();
 });
 
+clearEntryBtn.addEventListener("click", () => {
+  if (firstNum && !operator && !gottenResult) {
+    firstNum = "";
+    display.textContent = "0";
+  } else if (firstNum && operator && !secondNum) {
+    display.textContent = "0";
+  } else if (secondNum && operator) {
+    secondNum = "";
+    display.textContent = "0";
+  } else if (gottenResult) {
+    gottenResult = false;
+    firstNum = "";
+    secondNum = "";
+    operator = "";
+    symbol.textContent = "";
+    display.textContent = "0";
+  }
+});
+
 operateBtns.forEach((element) => {
   element.addEventListener("click", (e) => {
     if (firstNum && secondNum && operator) {
-      const result = operate(firstNum, secondNum, operator);
+      //check if the user is trying to divide by 0
+      if (operator === "division" && secondNum === "0") {
+        alert("You can't divide by 0!");
+        return;
+      }
+      let result = operate(firstNum, secondNum, operator);
+
+      if (!Number.isInteger(result)) {
+        result = result.toFixed(2); // round to 2 decimal places
+      }
       display.textContent = result;
       firstNum = result;
       symbol.textContent = `${symbol.textContent} ${secondNum} = `;
       secondNum = "";
+    }
+    if (!firstNum) {
+      return;
     }
     operator = e.target.value;
     symbol.textContent = `${firstNum} ${e.target.textContent}`;
@@ -86,7 +119,10 @@ equalBtn.addEventListener("click", () => {
       alert("You can't divide by 0!");
       return;
     }
-    const result = operate(firstNum, secondNum, operator);
+    let result = operate(firstNum, secondNum, operator);
+    if (!Number.isInteger(result)) {
+      result = result.toFixed(2); // round to 2 decimal places
+    }
     display.textContent = result;
     firstNum = result;
     gottenResult = true;
