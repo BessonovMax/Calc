7f842db914813a257c48b141eed46af2fc69036a
Maks Bessonov <max8essonov@gmail.com>
2025-04-01 13:41:28 +0500

    refactor calculator logic and improve button styles

--- main.js
+++ main.js
@@ -1,47 +1,12 @@
-let firstNum = null;
-let secondNum = null;
-let operator = null;
-
+let firstNum = "";
+let secondNum = "";
+let operator = "";
 let display = document.querySelector(".display");
 let equalBtn = document.querySelector(".equal");
 let numBtns = document.querySelectorAll(".number");
 let operateBtns = document.querySelectorAll(".operator");
 let clearBtn = document.querySelector(".clear-btn");
 let symbol = document.querySelector(".operator-symbol");
-
-clearBtn.addEventListener("click", () => {
-  window.location.reload();
-});
-
-operateBtns.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    console.log(e.target.value);
-    operator = e.target.value;
-    symbol.textContent = e.target.textContent;
-  });
-});
-
-numBtns.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    if (!firstNum) {
-      firstNum = Number(e.target.value);
-    } else {
-      secondNum = Number(e.target.value);
-    }
-    console.log(e.target.value);
-    display.textContent = e.target.value;
-  });
-});
-
-equalBtn.addEventListener("click", () => {
-  if (firstNum && secondNum && operator) {
-    display.textContent = operate(firstNum, secondNum, operator);
-    firstNum = operate(firstNum, secondNum, operator);
-  } else {
-    /* should to rewrite this logic, like in windows' calculator */
-    throw new Error("you must choose fist number, operator, second number");
-  }
-});
 
 function addNumbers(a, b) {
   return a + b;
@@ -60,6 +25,8 @@ function divideNumbers(a, b) {
 }
 
 function operate(num1, num2, operator) {
+  num1 = Number(num1);
+  num2 = Number(num2);
   switch (operator) {
     case "addition":
       return addNumbers(num1, num2);
@@ -71,3 +38,49 @@ function operate(num1, num2, operator) {
       return multiplyNumbers(num1, num2);
   }
 }
+
+clearBtn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+operateBtns.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    if (firstNum && secondNum && operator) {
+      const result = operate(firstNum, secondNum, operator);
+      display.textContent = result;
+      firstNum = result;
+      symbol.textContent = `${symbol.textContent} ${secondNum} = `;
+      secondNum = "";
+    }
+    operator = e.target.value;
+    symbol.textContent = `${firstNum} ${e.target.textContent}`;
+  });
+});
+
+numBtns.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    if (!operator) {
+      firstNum = firstNum + e.target.value;
+      display.textContent = firstNum;
+    } else {
+      secondNum = secondNum + e.target.value;
+      display.textContent = secondNum;
+    }
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  if (firstNum && operator) {
+    secondNum = display.textContent;
+  }
+  if (firstNum && secondNum && operator) {
+    const result = operate(firstNum, secondNum, operator);
+    display.textContent = result;
+    firstNum = result;
+    symbol.textContent = `${symbol.textContent} ${secondNum} = `;
+    secondNum = "";
+  } else {
+    /* should to rewrite this logic, like in windows' calculator */
+    throw new Error("you must choose fist number, operator, second number");
+  }
+});
